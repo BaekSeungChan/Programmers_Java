9fe54b28b3ba3ba3d7eb7eda83e0548e18350ab7
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2025-08-11 22:14:37 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
+
 class Solution {
-    public String[] solution(String[] names) {
-        // 그룹 수 계산 (5명씩 묶으니 +4 해서 올림 처리)
-        int groupCount = (names.length + 4) / 5;
-        String[] answer = new String[groupCount];
-        
-        int index = 0;
-        for (int i = 0; i < names.length; i += 5) { // 5명씩 건너뛰기
-            answer[index++] = names[i]; // 각 그룹의 첫 번째 사람 저장
+    public ArrayList<String> solution(String[] names) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(int i = 0; i < names.length; i++){
+            if(i % 5 == 0){
+                answer.add(names[i]);
+            }
         }
         
         return answer;
